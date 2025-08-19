69ae6f55f1dd18bedc0c0e38e4f403693ee02f8a
Nepein Andrey <nepein96@gmail.com>
2025-08-20 01:42:40 +0300

    feat: update public API exports
    - Export new createBounceScrollOffsetInterpolator
    - Export new types: PageStyleInterpolator, ScrollOffsetInterpolator
    - Export updated types: OverscrollSide, PagerStyleFn
    - Remove old PageInterpolator export

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 export { PagerView } from './PagerView';
 export { OnscreenPage } from './OnscreenPage';
 export { useIsOnscreenPage } from './context';
+export {
+  createBounceScrollOffsetInterpolator,
+  type BounceScrollOffsetInterpolatorConfig,
+} from './interpolators/bounceScrollOffsetInterpolator';
 export type {
   PagerViewProps,
   PagerViewRef,
   ScrollState,
-  PageInterpolator,
+  PageStyleInterpolator,
+  ScrollOffsetInterpolator,
+  Orientation,
+  ScrollPosition,
+  PageStyleInterpolatorParams,
+  ScrollOffsetInterpolatorParams,
+  OverscrollSide,
+  PagerStyleFn,
 } from './types';
